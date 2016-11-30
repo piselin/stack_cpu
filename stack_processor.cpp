9951f4e285f5d8311ca0c34043e31852a8885efc
Pi <iselinp@student.ethz.ch>
2016-11-30 12:22:37 +0100

    part a almost done. Need to fix the output of disassembly and change into a class

--- stack_processor.cpp
+++ stack_processor.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
 
+#include "disassembly.hpp"
 
 /*
 Disassembly: 
@@ -20,18 +23,14 @@ The simulated system will execute the machine instructions
 
 using instruction_in_t = unsigned int;
 
-
-//lookup function
-
-// has to store
-// opcode
-// operand
-// provide functionality to split
 class Instruction {
 public:
 	Instruction(const instruction_in_t in) 
 	: _in(in) 
 	{
+		//1. check the input
+		check();
+		//2. take it appart into opcode & operand
 		split();
 	}
 
@@ -48,6 +47,7 @@ public:
 	}
 private:
 	inline bool split();
+	inline bool check();
 
 private:
 	instruction_in_t _in;
@@ -55,48 +55,87 @@ private:
 	int _operand;
 };
 
-
+// wrapper to print an Instruction in an easy way
 std::ostream& operator<<(std::ostream& os, const Instruction& in) {
 	in.print(os);
 	return os;
 }
 
+bool Instruction::check() {
+	return true;
+}
 
 bool Instruction::split() {
 
 	std::cout << "splitting " << _in << std::endl;
 
+
+
 	return true;
 }
 
-int main() {
-	// std::string mode_input;
-	// std::cin >> mode_input;
-
-	// enum Mode {disassemble, simulation};
-
-	// //1. figure out which we are in
-	// if(mode_input == "disassemble") {
-	// 	// enter disassemble mode
-	// 	Mode current_mode = disassemble;
-	// }
-
-	//2. read number of instructions
-	int n = 0;
-	std::cin >> n;
-
-	std::cin >> std::hex; // set input to hex mode
-
-	for(int instruction_cnt = 0; instruction_cnt < n; instruction_cnt++) {
-		unsigned int input; // incoming instruction
-		std::cin >> input;
-		Instruction in(input);
-		//std::cout << in << std::endl;
+// test this with gtest
+std::string dec_to_hex(unsigned int dec) {
+	const std::string hex_base = "0123456789abcdef";
+	std::string hex;
+	while(dec > 0) {
+		unsigned int r = dec % 16;
+		hex += hex_base[r];
+		dec /= 16;
 	}
-	//3. process the input 
-	//3.1 for each input:
-	//	decode the input
-	// 	lookup the op code
-	// 	output
-	// 		
+	std::reverse(hex.begin(), hex.end());
+	return hex;
+}
+
+unsigned int get_op_code(std::string hex) {
+	unsigned int opcode = 0;
+		
+	// doesn't work...
+	// opcode += std::stoi(hex[0])*16;
+	// opcode += std::stoi(hex[1]);
+	return opcode;
+}
+
+int main()
+{
+    std::string command;
+    std::cin >> command;
+
+    // 1. which mode are we in
+    if (command == "disassemble") {
+       
+        // 2. read number of instructions
+		int n = 0;
+		std::cin >> n;
+
+		std::cin >> std::hex; // set input to hex mode
+
+		unsigned int hex = 0;
+		for (unsigned int i = 0; i < n; i++) {
+			std::cin >> hex;
+			unsigned int opcode = 0;
+			unsigned int operand = 0;
+
+			Decode(hex, opcode, operand);
+
+			// fixme pi: output should only print operand for "opcode c";
+			std::cout 	<< MapOpCodeToInstruction(opcode) << " " 
+						<< std::dec << operand 
+						<< std::endl;
+		}
+
+		//3. process the input 
+		//3.1 for each input:
+		//	decode the input
+		// 	lookup the op code
+		// 	output
+		// 		
+        
+    } else if (command == "simulate") {
+        
+        // TODO: perform simulation task b)
+        
+    } else {
+        std::cerr << "unkown command\n";
+    }
 }
